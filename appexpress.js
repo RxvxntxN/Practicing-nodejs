727f3395cf440415b65a6a8cea5517473f478683
Muhammad Musabbir <musabbir666@gmail.com>
2026-02-25 23:08:33 +0600

    going to add how data can be pushed

--- appexpress.js
+++ appexpress.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+const rootDir = require('./util/path');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
-
-const adminRoutes = require('./routes/admin');
+//const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const userRoutes = require('./routes/users');
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(userRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
+    //res.sendFile(path.join(rootDir, 'views', 'users.html'));
+    console.log(adminData.users);
+    res.render('users', {users: adminData.users, pageTitle: 'Users'});
 });
 
 module.exports = router;
